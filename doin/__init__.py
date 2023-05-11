c45e159146ea15d74b62d353a4b4c0a0a8e527f1
Hannes <hannes@lohmander.org>
2023-05-11 17:39:01 +0900

    Add build function to top level package

--- doin/__init__.py
+++ doin/__init__.py
@@ -1,7 +1,8 @@
 """Top-level package for DOIN."""
 from doin.preprocess import preprocess
+from doin.doin import build_model_from_clip
 
-__all__ = ["preprocess"]
+__all__ = ["preprocess", "build_model_from_clip"]
 __author__ = """Hannes Lohmander"""
 __email__ = "hannes@lohmander.org"
 __version__ = "0.1.0"
